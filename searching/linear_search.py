577972e9277feb33102a1ab48772d16434e9dc6f
minaradan006 <minaradan006@minaradan006@gmail.com>
2026-07-10 06:40:00 -0400

    docs: added docstring with time and space complexity

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -2,11 +2,23 @@ from typing import List, TypeVar
 
 T = TypeVar('T', int, float, str)
 
-def linear_search(arr: List[T], num: T) -> int:
+def linear_search(arr: List[T], elem: T) -> int:
+	"""
+	Searches for a target element within a list sequentially.
+
+	Time Complexity: O(n)
+	Space Complexity: O(1)
+
+	Args:
+		arr (List[T]): A list of elements to search through
+		elem (T): The target element to locate.
+	Returns:
+		int: The index of the element if found, otherwise -1.
+	"""
 	n: int = len(arr)
 
 	for idx in range(n):
-		if arr[idx] == num:
+		if arr[idx] == elem:
 			return idx
 
 	return -1
